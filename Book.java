eb54bf1660c51efb8b428d3e662e9dad39879620
RachanaChiddamchetti11 <161217616+RachanaChiddamchetti11@users.noreply.github.com>
2024-02-25 18:54:15 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,63 @@
+package javalibraryproject;
+
+public class Book {
+	private String title;
+	private int id;
+	private int cost;
+	private String author;
+	private int noOfPages;
+
+	public Book() {
+
+	}
+
+	public Book(String title, int id, int cost, String author, int noOfPages) {
+		super();
+		this.title = title;
+		this.id = id;
+		this.cost = cost;
+		this.author = author;
+		this.noOfPages = noOfPages;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getNoOfPages() {
+		return noOfPages;
+	}
+
+	public void setNoOfPages(int noOfPages) {
+		this.noOfPages = noOfPages;
+	}
+
+}
